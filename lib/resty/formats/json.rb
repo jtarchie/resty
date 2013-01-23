78738c91d0baee7771629c137daa146311471ca9
JT Archie <jtarchie@gmail.com>
2013-01-23 18:40:47 -0500

    JSON responds to format at end of line

--- lib/resty/formats/json.rb
+++ lib/resty/formats/json.rb
@@ -2,7 +2,7 @@ module Resty
   module Formats
     class JSON < Base
       def self.matches?(request)
-        request.path =~ /\.json/
+        request.path =~ /\.json$/
       end
 
       def body
